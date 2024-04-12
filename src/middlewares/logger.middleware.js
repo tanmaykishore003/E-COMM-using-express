820c1097ad7da7e05222ba485f2dd26261563b22
tanmaykishore003 <tanmaykishore003@gmail.com>
2024-04-12 09:36:46 +0530

    Add Winston logger

--- src/middlewares/logger.middleware.js
+++ src/middlewares/logger.middleware.js
@@ -1,20 +1,34 @@
 
 import fs from 'fs'
+import winston from 'winston';
 
-const fsPromise = fs.promises;
+// const fsPromise = fs.promises;
 
-async function log(logData) {
-    try {
-        logData = `\n ${new Date().toString()} - ${logData}`
-        await fsPromise.appendFile("log.txt", logData);
-    }catch(err) {
-        console.log(err);
-    }
-}
+// async function log(logData) {
+//     try {
+//         logData = `\n ${new Date().toString()} - ${logData}`
+//         await fsPromise.appendFile("log.txt", logData);
+//     }catch(err) {
+//         console.log(err);
+//     }
+// }
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: {
+        service: 'request-logging'
+    },
+    transports: [
+        new winston.transports.File({filename: 'log.txt'})
+    ]
+})
 
 const loggerMiddleware = async (req, res, next) => {
-    const logData = `${req.url} - ${JSON.stringify(req.body)}`
-    await log(logData);
+    if(!req.url.includes('signIn')) {
+        const logData = `${req.url} - ${JSON.stringify(req.body)}`
+        logger.info(logData)
+    }
     next();
 };
 
